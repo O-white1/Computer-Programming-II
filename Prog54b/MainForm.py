bddc86ed99394618121205d613d9994f3ef12f5d
O-white1 <white.o3@stu.janesville.k12.wi.us>
2024-01-25 14:42:31 -0600

    1/25/24 2:42 PM GD2 Final

---
+++ Prog54b/MainForm.py
@@ -0,0 +1,135 @@
+﻿import System.Drawing
+import System.Windows.Forms
+
+from System.Drawing import *
+from System.Windows.Forms import *
+
+class MainForm(Form):
+	def __init__(self):
+		self.InitializeComponent()
+	
+	def InitializeComponent(self):
+		self._label1 = System.Windows.Forms.Label()
+		self._textBox1 = System.Windows.Forms.TextBox()
+		self._textBox2 = System.Windows.Forms.TextBox()
+		self._textBox3 = System.Windows.Forms.TextBox()
+		self._textBox4 = System.Windows.Forms.TextBox()
+		self._label2 = System.Windows.Forms.Label()
+		self._label3 = System.Windows.Forms.Label()
+		self._button1 = System.Windows.Forms.Button()
+		self._button2 = System.Windows.Forms.Button()
+		self._button3 = System.Windows.Forms.Button()
+		self.SuspendLayout()
+		# 
+		# label1
+		# 
+		self._label1.BackColor = System.Drawing.Color.White
+		self._label1.Font = System.Drawing.Font("Microsoft Uighur", 26.25, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, 0)
+		self._label1.Location = System.Drawing.Point(17, 7)
+		self._label1.Name = "label1"
+		self._label1.Size = System.Drawing.Size(261, 65)
+		self._label1.TabIndex = 0
+		self._label1.Text = "Enter 4 Numbers:"
+		# 
+		# textBox1
+		# 
+		self._textBox1.BackColor = System.Drawing.Color.White
+		self._textBox1.Location = System.Drawing.Point(82, 78)
+		self._textBox1.Name = "textBox1"
+		self._textBox1.Size = System.Drawing.Size(100, 20)
+		self._textBox1.TabIndex = 1
+		self._textBox1.TextChanged += self.TextBox1TextChanged
+		# 
+		# textBox2
+		# 
+		self._textBox2.BackColor = System.Drawing.Color.White
+		self._textBox2.Location = System.Drawing.Point(82, 104)
+		self._textBox2.Name = "textBox2"
+		self._textBox2.Size = System.Drawing.Size(100, 20)
+		self._textBox2.TabIndex = 2
+		# 
+		# textBox3
+		# 
+		self._textBox3.BackColor = System.Drawing.Color.White
+		self._textBox3.Location = System.Drawing.Point(82, 130)
+		self._textBox3.Name = "textBox3"
+		self._textBox3.Size = System.Drawing.Size(100, 20)
+		self._textBox3.TabIndex = 3
+		# 
+		# textBox4
+		# 
+		self._textBox4.BackColor = System.Drawing.Color.White
+		self._textBox4.Location = System.Drawing.Point(82, 156)
+		self._textBox4.Name = "textBox4"
+		self._textBox4.Size = System.Drawing.Size(100, 20)
+		self._textBox4.TabIndex = 4
+		# 
+		# label2
+		# 
+		self._label2.BackColor = System.Drawing.Color.White
+		self._label2.Font = System.Drawing.Font("Microsoft Uighur", 26.25, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, 0)
+		self._label2.Location = System.Drawing.Point(7, 179)
+		self._label2.Name = "label2"
+		self._label2.Size = System.Drawing.Size(137, 55)
+		self._label2.TabIndex = 5
+		self._label2.Text = "Average:"
+		# 
+		# label3
+		# 
+		self._label3.BackColor = System.Drawing.Color.White
+		self._label3.Font = System.Drawing.Font("Microsoft Uighur", 15.75, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, 0)
+		self._label3.Location = System.Drawing.Point(139, 179)
+		self._label3.Name = "label3"
+		self._label3.Size = System.Drawing.Size(158, 55)
+		self._label3.TabIndex = 6
+		self._label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+		# 
+		# button1
+		# 
+		self._button1.Location = System.Drawing.Point(7, 237)
+		self._button1.Name = "button1"
+		self._button1.Size = System.Drawing.Size(290, 57)
+		self._button1.TabIndex = 7
+		self._button1.Text = "Calc"
+		self._button1.UseVisualStyleBackColor = True
+		# 
+		# button2
+		# 
+		self._button2.Location = System.Drawing.Point(7, 300)
+		self._button2.Name = "button2"
+		self._button2.Size = System.Drawing.Size(290, 56)
+		self._button2.TabIndex = 8
+		self._button2.Text = "Clear"
+		self._button2.UseVisualStyleBackColor = True
+		# 
+		# button3
+		# 
+		self._button3.Location = System.Drawing.Point(7, 362)
+		self._button3.Name = "button3"
+		self._button3.Size = System.Drawing.Size(290, 67)
+		self._button3.TabIndex = 9
+		self._button3.Text = "Exit"
+		self._button3.UseVisualStyleBackColor = True
+		# 
+		# MainForm
+		# 
+		self.BackColor = System.Drawing.Color.Maroon
+		self.ClientSize = System.Drawing.Size(845, 460)
+		self.Controls.Add(self._button3)
+		self.Controls.Add(self._button2)
+		self.Controls.Add(self._button1)
+		self.Controls.Add(self._label3)
+		self.Controls.Add(self._label2)
+		self.Controls.Add(self._textBox4)
+		self.Controls.Add(self._textBox3)
+		self.Controls.Add(self._textBox2)
+		self.Controls.Add(self._textBox1)
+		self.Controls.Add(self._label1)
+		self.Name = "MainForm"
+		self.Text = "Prog54b"
+		self.ResumeLayout(False)
+		self.PerformLayout()
+
+
+	def TextBox1TextChanged(self, sender, e):
+		pass
