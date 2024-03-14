3a7a6dda3cbb95e4486a146aa62b8920d6df3bb5
O-white1 <white.o3@stu.janesville.k12.wi.us>
2024-03-14 14:35:22 -0500

    2:35 yodate

--- CL213f.py
+++ CL213f.py
@@ -1,24 +1,13 @@
 class Cl213f:
-
-    def __int__(self, kwh):
+    def __int__(self, kwh, cost):
         self.kwh = kwh
         self.cost = 0.0
     def calc(self):
-        if self.kwh <= 2000:
-            self.cost = self.kwh * 0.07
-        elif 2000 < self.kwh < 10000:
-            self.cost = 2000 * 0.07 + (self.kwh - 2000)*0.05
-
-
-
-        """
         no = 1
         while no < self.kwh:
-            while no <= 2000: cost = 0.07
-            while no <= 10000: cost = 0.05
-            while no > 10000: cost = 0.04
-            bill = kwh*cost
-        """
+            while no <=  2000: self.cost = 0.07
+            while no <= 10000: self.cost = 0.05
+            while no >  10000: self.cost = 0.04
 
 
 
