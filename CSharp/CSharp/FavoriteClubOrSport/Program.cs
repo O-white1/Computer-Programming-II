7f539362bd7d071e161546a3150cab277e473625
O-white1 <white.o3@stu.janesville.k12.wi.us>
2024-04-08 14:41:25 -0500

    update(4/8/24 C# begins)

---
+++ CSharp/CSharp/FavoriteClubOrSport/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FavoriteClubOrSport
+{
+    class Program {
+        static void Main(string[] args) {
+            Console.Write("Favrite Sport or Club: ");
+            string ClubOrSport = Convert.ToString(Console.ReadLine());
+            Console.WriteLine("Your favorite Sport/Club is " + ClubOrSport + "?  What a loser!");
+        }
+    }
+}
