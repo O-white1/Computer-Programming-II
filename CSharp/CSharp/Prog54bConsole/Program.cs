fa60fc1534a5f57f128daea66820dffc49f6e7fc
O-white1 <white.o3@stu.janesville.k12.wi.us>
2024-04-10 14:40:04 -0500

    4/10/24 [C# programs done] 52a, 54b, 54c

---
+++ CSharp/CSharp/Prog54bConsole/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prog54bConsole {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Enter 4 numbers to get SUM and AVG");
+
+            int num1 = int.Parse(Console.ReadLine());
+            int num2 = int.Parse(Console.ReadLine());
+            int num3 = int.Parse(Console.ReadLine());
+            int num4 = int.Parse(Console.ReadLine());
+
+            int sum = num1 + num2 + num3 + num4;
+            double avg = (double)sum / 4; // 4.0
+
+            Console.WriteLine("Sum: " + sum);
+            Console.WriteLine("Average: " + Math.Round(avg, 2));
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ CSharp/CSharp/Prog54cConsole/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prog54cConsole {
+    class Program {
+        static void Main(string[] args) {
+
+            Console.Write("Enter Radius: ");
+            double rad = double.Parse(Console.ReadLine());
+            double area = (rad * rad) * 3.14159;
+            double circ = 2 * rad * 3.14159;
+
+            Console.WriteLine("Area: " + area);
+            Console.WriteLine("Circumpherence: " + circ);
+            Console.ReadKey();
+
+        }
+    }
+}
