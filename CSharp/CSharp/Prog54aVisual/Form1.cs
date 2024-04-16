dc9db8fd46d53d5b2b2898831c3c50d701b6ac4c
O-white1 <white.o3@stu.janesville.k12.wi.us>
2024-04-16 14:38:36 -0500

    update(4/16/24) [prog88aCS] complete
    several C# programs left to be done; see google classroom and infinite campus

--- CSharp/CSharp/Prog54aVisual/Form1.cs
+++ CSharp/CSharp/Prog54aVisual/Form1.cs
@@ -17,11 +17,11 @@ namespace Prog54aVisual {
             int gallons = 0; 
             double mpg = 0;
 
-            if (comboBox1.Text == "1970 VW Bug")        { miles = 286; gallons = 9;  }
+            if      (comboBox1.Text == "1970 VW Bug")   { miles = 286; gallons = 9;  }
             else if (comboBox1.Text == "1979 Firebird") { miles = 412; gallons = 40; }
             else if (comboBox1.Text == "1980 Subaru")   { miles = 361; gallons = 18; }
             else if (comboBox1.Text == "1975 Cutlass")  { miles = 161; gallons = 11; }
-            else { MessageBox.Show("Invalid car selection!");}  return;
+            else { MessageBox.Show("Invalid car selection!"); } return;
 
             mpg = (double)miles / gallons;
             mpg = Math.Round(mpg, 1);
---
+++ CSharp/CSharp/Prog88aCS/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Prog88aCS
+{
+    public partial class Form1 : Form {
+        public Form1() { InitializeComponent(); }
+
+        private void button1_Click(object sender, EventArgs e) {
+
+            int num1 = int.Parse(textBox1.Text);
+            int num2 = int.Parse(textBox2.Text);
+
+            int    sum = num1 + num2;
+            int    dif = num1 - num2;
+            int    prod = num1 * num2;
+            int    quot = num1 / num2; // unused
+            double avg = (double)sum / 2; // sum / 2.0;
+            int    abs = Math.Abs(dif);
+            int    max = 0;
+            int    min = 0;
+            if   (num1 > num2)  max = num1;
+            else  max = num2;
+            if   (num1 <= num2) min = num1;
+            else min = num2;
+
+            lblSum.Text = sum.ToString();
+            lblDif.Text = dif.ToString();
+            lblProd.Text = prod.ToString();
+            lblAvg.Text = avg.ToString();
+            lblAbsDif.Text = abs.ToString();
+            lblMax.Text = max.ToString();
+            lblMin.Text = min.ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e) { lblSum.Text = ""; lblDif.Text = ""; lblProd.Text = ""; lblAvg.Text = ""; lblAbsDif.Text = ""; lblMax.Text = ""; lblMin.Text = ""; }
+    }
+}
