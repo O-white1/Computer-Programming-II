c1fbfdc9937340604f582669bd66aa61e75a53b9
Owen White <white.o3@stu.janesville.k12.wi.us>
2024-04-22 14:44:16 -0500

    update(4/22/24) [2:43]
    repo has been cloned and c# programs are being worked on.

--- CSharp/CSharp/Prog54b_Form/Form1.Designer.cs
+++ CSharp/CSharp/Prog54b_Form/Form1.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace Prog85c
+﻿namespace Prog54b_Form
 {
     partial class Form1
     {
@@ -28,83 +28,59 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.textBox1 = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
-            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(243, 7);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(80, 20);
-            this.textBox1.TabIndex = 0;
-            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // label1
             // 
-            this.label1.BackColor = System.Drawing.SystemColors.ActiveBorder;
-            this.label1.Location = new System.Drawing.Point(63, 55);
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(345, 125);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "1.  Determine your birth month  (January = 1, February = 2, March = 3, …).";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(32, 196);
+            this.button1.Location = new System.Drawing.Point(12, 184);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(99, 81);
-            this.button1.TabIndex = 2;
+            this.button1.Size = new System.Drawing.Size(114, 65);
+            this.button1.TabIndex = 1;
             this.button1.Text = "Calculate";
             this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(157, 196);
+            this.button2.Location = new System.Drawing.Point(132, 184);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(129, 81);
-            this.button2.TabIndex = 3;
-            this.button2.Text = "Clear";
+            this.button2.Size = new System.Drawing.Size(248, 65);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Calculate";
             this.button2.UseVisualStyleBackColor = true;
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(312, 196);
+            this.button3.Location = new System.Drawing.Point(386, 184);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(96, 81);
-            this.button3.TabIndex = 4;
-            this.button3.Text = "Quit";
+            this.button3.Size = new System.Drawing.Size(114, 65);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Calculate";
             this.button3.UseVisualStyleBackColor = true;
-            // 
-            // label2
-            // 
-            this.label2.BackColor = System.Drawing.Color.Gray;
-            this.label2.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.label2.Location = new System.Drawing.Point(32, 41);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(376, 139);
-            this.label2.TabIndex = 5;
-            this.label2.Text = " 1.  Determine your birth month  (January = 1, February = 2, March = 3, …).";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(420, 301);
-            this.Controls.Add(this.label2);
+            this.ClientSize = new System.Drawing.Size(512, 261);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.textBox1);
             this.Name = "Form1";
             this.Text = "Form1";
-            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -112,12 +88,10 @@
 
         #endregion
 
-        private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Label label2;
     }
 }
 
