bc5e44ced9672abe7197350132f0f3c3e749bd8d
Owen White <white.o3@stu.janesville.k12.wi.us>
2024-04-23 14:04:27 -0500

    Update(4/23/24)
    LP4-2 completed as a class

---
+++ CSharp/CSharp/LP4-2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LP4_2 {
+    class Program {
+        static void Main(string[] args) {
+            Console.Write("Enter Weight: ");  int weight = int.Parse(Console.ReadLine());
+            Console.Write("Enter Length: ");  int length = int.Parse(Console.ReadLine());
+            Console.Write("Enter Height: ");  int height = int.Parse(Console.ReadLine());
+            Console.Write("Enter width: ");  int  width = int.Parse(Console.ReadLine());
+            int volume = length * weight * height;
+
+            if (weight > 27 && volume > 100000) Console.WriteLine("Package is too heavy and too large!");
+            else if (weight > 27)               Console.WriteLine("Pacakge is too heavy!");
+            else if (volume > 100000)           Console.WriteLine("Pacakge is too large!");
+            else                                Console.WriteLine("You're good to go!");
+            Console.ReadKey();
+        }
+    }
+}
