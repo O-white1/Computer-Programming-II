0351037f5ede83e4b642abcb431a6840250fb376
Owen White <white.o3@stu.janesville.k12.wi.us>
2024-04-26 14:08:46 -0500

    Update(4/26/24)
    Prog54b Form finished alone in C#.

--- CSharp/CSharp/Prog54b_Form/Form1.cs
+++ CSharp/CSharp/Prog54b_Form/Form1.cs
@@ -11,8 +11,15 @@ using System.Windows.Forms;
 namespace Prog54b_Form {
     public partial class Form1 : Form {
         public Form1() { InitializeComponent(); }
+
         private void button1_Click(object sender, EventArgs e) {
-            // TODO: 54b form stuff
+            int num1 = int.Parse(textBox1.Text);
+            int num2 = int.Parse(textBox2.Text);
+            int sum = (num1 + num2);
+            double avg = (double)sum / 2;
+            label1.Text = ("Sum: " + sum + " Average: " + avg);
         }
+        private void button2_Click(object sender, EventArgs e) { label3.Text = ""; label2.Text = ""; label1.Text = ""; }
+        private void button3_Click(object sender, EventArgs e) { Application.Exit(); }
     }
 }
