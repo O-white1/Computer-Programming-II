0628e8e3de1a1958c51302a1f45652e39dded805
Owen White <white.o3@stu.janesville.k12.wi.us>
2024-05-03 14:14:01 -0500

    Update(5/3/24)
    Finished prog122d alone

--- CSharp/CSharp/Prog122d/Form1.cs
+++ CSharp/CSharp/Prog122d/Form1.cs
@@ -13,7 +13,10 @@ namespace Prog122d {
         public Form1()
             { InitializeComponent(); }
         private void button2_Click(object sender, EventArgs e) {
-            
+            for (int x = -16; x <= 12; x++) {
+                double y = Math.Pow(x, 6) - (3 * (Math.Pow(x, 5))) - (93 * (Math.Pow(x, 4))) + (87 * (Math.Pow(x, 3))) + (1596 * (Math.Pow(x, 2))) - (1380 * x) - 2800;
+                listBox1.Items.Add(x + "\t\t" + y);
+            }
         }
         private void button1_Click(object sender, EventArgs e) { listBox1.Items.Clear(); }
         private void button3_Click(object sender, EventArgs e) { Application.Exit(); }
