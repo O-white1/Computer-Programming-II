dd5a9ccb86619c01ba8e1f16366aa48b94f680a7
Owen White <white.o3@stu.janesville.k12.wi.us>
2024-05-09 14:36:27 -0500

    Update(5/9/24)
    do the stuff i suppose

---
+++ CSharp/CSharp/CatchMe535REAL/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CatchMe535REAL {
+    public partial class Form1 : Form {
+        public Form1() { InitializeComponent(); }
+        private void button1_Click(object sender, EventArgs e) { Application.Exit(); }
+        public void Move() {
+            int wid = System.Windows.Form.Size; // fix this crap
+        }
+
+        public static void main(String[] args) {
+
+        }
+
+
+        public static int Length { get; set; }
+    }
+}
