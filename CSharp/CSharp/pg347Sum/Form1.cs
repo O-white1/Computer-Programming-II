d49d73d32edf0b9c1ec15d1f9d09f68912f28e65
Owen White <white.o3@stu.janesville.k12.wi.us>
2024-05-09 14:14:40 -0500

    Update(5/9/24) [pg347Sum]

--- CSharp/CSharp/pg347Sum/Form1.cs
+++ CSharp/CSharp/pg347Sum/Form1.cs
@@ -10,12 +10,15 @@ using System.Windows.Forms;
 using Microsoft.VisualBasic;
 namespace pg347Sum {
     public partial class Form1 : Form {
-        public Form1() {
-            InitializeComponent();
-        }
+        public Form1() 
+            { InitializeComponent(); }
+        private void button1_Click(object sender, EventArgs e) { Application.Exit(); }
         private void button2_Click(object sender, EventArgs e) {
-            string variable = Interaction.InputBox("Prompt Here", "Title");
-            MessageBox.Show(variable);
+            int total = 0;
+            int variable = int.Parse(Interaction.InputBox("Prompt Here", "Title"));
+            for (int lcv = 0; lcv <= variable; lcv++)
+                total += lcv;
+            MessageBox.Show(total.ToString());
         }
     }
 }
