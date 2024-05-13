ef0b8c993a50d74697f6cc168c7389c481362493
Owen White <white.o3@stu.janesville.k12.wi.us>
2024-05-13 13:48:47 -0500

    update(yes)

--- CSharp/CSharp/CatchMe535REAL/Form1.cs
+++ CSharp/CSharp/CatchMe535REAL/Form1.cs
@@ -7,20 +7,31 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using Random;
 namespace CatchMe535REAL {
     public partial class Form1 : Form {
-        public Form1() { InitializeComponent(); }
-        private void button1_Click(object sender, EventArgs e) { Application.Exit(); }
-        public void Move() {
-            int wid = System.Windows.Form.Size; // fix this crap
+        public Form1() 
+            { InitializeComponent(); }
+
+        private void Move() {
+            System.Threading.Thread.Sleep(3000);
+            Form myForm = this.ParentForm;
+            int length = myForm.Height;
+            int width = myForm.Width;
+            Random rndL = new Random(); int newl = rndL.Next(0, length);
+            Random rndW = new Random(); int newr = rndW.Next(0, width);
+
+            myForm.button1.Location.X = (rndW);
+            button1.Location.Y = (rndL);
         }
-
-        public static void main(String[] args) {
-
+       private void main() {
+           this.Move();       
         }
+       private void button1_Click(object sender, EventArgs e) { Application.Exit(); }
 
+       private void Form1_Load(object sender, EventArgs e)
+       {
 
-        public static int Length { get; set; }
+       }
     }
 }
